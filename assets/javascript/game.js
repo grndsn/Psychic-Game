3f3cb22cabadb758cd5e5ce52e2df749a754ec1f
Phil Morgan <philip.morgan1194@gmail.com>
2017-06-26 16:56:25 -0500

    This is my new commit msg from terminal

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// this line sets up the variable for whiche ever letter choice the user tpyes
+var letterChoice =['a', 'b', 'c', 'd', 'f', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+//setting these varibales to zero
+var wins = 0;
+var loses = 0;
+var guessLeft = 9;
+var guessedLetters = 9;
+var guessesSofar = null; {
+	document.write()
+}
